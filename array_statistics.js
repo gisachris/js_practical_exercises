cfe045d23e075b835701ac8c4fd37dd8ae4d2747
gisa chris <25574259+gisachris@users.noreply.github.com>
2024-11-06 13:17:16 +0200

    document the reduce method approach to solving the array statistics problem

--- array_statistics.js
+++ array_statistics.js
@@ -6,17 +6,17 @@ const arrayStats = (arr) => {
 
     //sum
     let sum = 0
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         sum += arr[i]
     }
     statObject.sum = sum
 
     //average
-    statObject.average = sum/arr.length
+    statObject.average = sum / arr.length
 
     //min
     statObject.min = Math.min(...arr)
-    
+
     //max
     statObject.max = Math.max(...arr)
 
@@ -28,9 +28,13 @@ console.log(arrayStats([1, 2, 3, 4, 5]))
 console.log(arrayStats([-10, -5, 0, 5, 10]))
 console.log(arrayStats([1, 1, 1, 1, 1]))
 
-//a better approach could have been to use the reduce method.
+//a better approach could have been to use the reduce method for sum and average.
 /*
 reduce method syntax:
+for the sum it could have been
 
+let sum  =  arr.reduce((accumulator,currentval)=>{accumulator + current val},0)
+
+let average  =  arr.reduce((accumulator,currentval)=>{accumulator + current val},0)/arr.length
 
 */
