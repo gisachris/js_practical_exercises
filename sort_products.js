34d2989b7fe13c7d9f0a925f36e98e6455e6ad6a
gisa chris <25574259+gisachris@users.noreply.github.com>
2024-11-06 14:28:23 +0200

    work on exercise 3 product sorting

--- sort_products.js
+++ sort_products.js
@@ -2,6 +2,9 @@
 //sort them in ascending order based on price
 
 const sortProducts = (products) => {
-    
-    return products;
+    let sorted_products = products.sort((a, b) => { b.price - a.price })
+    return sorted_products
 }
+
+//examples
+console.log(sortProducts([{ name: 'banana', price: 1 }, { name: 'apple', price: 2 }, { name: 'orange', price: 3 }]))
