3efd5e22bc4ce2d9197f297479de2769c76a01e2
gisa chris <125574259+gisachris@users.noreply.github.com>
2024-11-07 17:06:48 +0200

    implemet reduce function replica in myreducee function

--- array_methods.js
+++ array_methods.js
@@ -1,17 +1,17 @@
 //create array methods that replicate built-in array methods(map, filter, reduce, foreach)
 
 //map
-function mymapCallback(item) {
+function mapCallback(item) {
     //does an action to an element in the array
-    item++
+    item += 1
     return item
 }
-function myMap(mymapCallback) {
+function myMap(mapCallback) {
     let arr = [1, 2, 3]
     let newArray = [];
 
     for (let i = 0; i < arr.length; i++) {
-        newArray.push(mymapCallback(arr[i]))
+        newArray.push(mapCallback(arr[i]))
     }
 
     return newArray
@@ -37,9 +37,32 @@ function myFilter(filterCallback) {
 }
 
 //reduce
-function myreduce() { }
+function reduceCallback(accumulator, num) {
+    return accumulator + num
+}
+function myReduce(reduceCallback, initialVal = null) {
+    let arr = [1, 2, 3]
+    let accumulator = initialVal || arr[0]
+    let startIndex = 0
+
+    if (initialVal !== null) {
+        accumulator = initialVal
+    } else {
+        accumulator = arr[0]
+        startIndex = 1
+    }
+
+    for (let i = startIndex; i < arr.length; i++) {
+        accumulator = reduceCallback(accumulator, arr[i])
+    }
+
+    return accumulator
+}
 
 //foreach
 function myForEach() { }
 
 //examples
+console.log("mymap=>", myMap(mapCallback))
+console.log("myFilter=>", myFilter(filterCallback))
+console.log("myReduce=>", myReduce(reduceCallback))
