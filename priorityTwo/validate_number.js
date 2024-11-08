a8cab204d9cd8e27745734e813d90c38aeb1bab4
chris Gisa <125574259+gisachris@users.noreply.github.com>
2024-11-08 14:37:25 +0200

    create validate number function

---
+++ priorityTwo/validate_number.js
@@ -0,0 +1,10 @@
+// validate a number with these conditions
+/*
+- it must be exactly ten digits
+- it can contain hypens(-) or spaces at the 3rd and 6th digits
+*/
+
+function validateNumber() {
+    //check for reqs first
+    
+}
